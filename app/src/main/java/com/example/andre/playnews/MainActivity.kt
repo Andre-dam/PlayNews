c7f6757470f6aca27a8799ceff550d44f0557a12
Andre <aldsj@cin.ufpe.br>
2018-11-13 13:37:24 -0300

    Tab widget added

--- app/src/main/java/com/example/andre/playnews/MainActivity.kt
+++ app/src/main/java/com/example/andre/playnews/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.andre.playnews
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myToolbar = findViewById(R.id.actionbarid) as Toolbar
+        setSupportActionBar(myToolbar)
     }
 }
