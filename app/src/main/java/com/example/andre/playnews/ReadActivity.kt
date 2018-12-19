39ec6bed0a003a2d46b62682f9be4acd720611f6
Fabio Sobral <fnps@cin.ufpe.br>
2018-12-18 23:21:08 -0300

    Texto Justificado e levemente formatado

--- app/src/main/java/com/example/andre/playnews/ReadActivity.kt
+++ app/src/main/java/com/example/andre/playnews/ReadActivity.kt
@@ -4,10 +4,8 @@ import android.annotation.SuppressLint
 import android.os.AsyncTask
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
-import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import com.squareup.picasso.Picasso
@@ -27,7 +25,7 @@ class ReadActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         tts = TextToSpeech(this, this)
         setContentView(R.layout.activity_read)
         setSupportActionBar(toolbar)
-        fab.setOnClickListener { view ->
+        fab.setOnClickListener {
             if (!tts!!.isSpeaking) {
                 fab!!.setImageResource(R.drawable.ic_media_pause)
                 fab!!.hide()
@@ -38,8 +36,7 @@ class ReadActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
                     setSpeechRate(1.5F)
                     speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
                 }
-            }
-            else {
+            } else {
                 fab!!.setImageResource(R.drawable.ic_media_play)
                 fab!!.hide()
                 fab!!.show()
@@ -51,8 +48,8 @@ class ReadActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         val bundle = intent.extras
         if (bundle != null) {
             val readableItem = DownloadContent().execute(bundle.getString("link"), bundle.getString("url")).get()
-            rssContentTextBody.text = readableItem.mBody
-            if (readableItem.mImage != null && !"".equals(bundle.getString("url")))
+            rssContentTextBody.text = readableItem.mBody.replace("""(\s?|\s+)[.]+(\s+|\s?)""".toRegex(), ".\n\n")
+            if (readableItem.mImage != null && "" != bundle.getString("url"))
                 readableItem.mImage.into(mImage)
         }
     }
@@ -62,12 +59,24 @@ class ReadActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
         override fun doInBackground(vararg params: String?): ReadableItem {
             var crop: RequestCreator? = null
-            val body = Jsoup.connect(params[0]).get().body()
-            if (params[1] != null && !"".equals(params[1])) {
+            val selection = Jsoup.connect(params[0]).get().body()
+
+            selection.select("time, header, footer, head, nav, aside, span, #header, #head, #title, [class~=(?i).*description.*|(?i).*related.*]").remove()
+
+            selection.allElements.forEach {
+                val s = it.text()
+                if (it.tagName() in listOf("h1", "h2", "h3", "h4", "h5", "h6", "p", "li", "ul", "ol", "div")
+                        && !s.endsWith('.')
+                        && s.isNotEmpty()) {
+                    it.append(".")
+                }
+            }
+
+            if (params[1] != null && "" != params[1]) {
                 crop = Picasso.get().load(params[1]).resize(480, 180).centerCrop()
             }
-            val bodyContent = body.getElementsByTag("p").text()
-            return ReadableItem(crop, bodyContent)
+
+            return ReadableItem(crop, selection.select("[class~=(?i).*article.*], [itemprop~=(?i).*article.*], article").last().text())
         }
     }
 
@@ -89,7 +98,7 @@ class ReadActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     }
 
     override fun onDestroy() {
-        if (tts != null){
+        if (tts != null) {
             tts!!.stop()
             tts!!.shutdown()
         }
